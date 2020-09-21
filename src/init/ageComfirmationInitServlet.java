0a2e12e2b8387c803cf100f293ce47147e2b6735
KeishiFukuda <71387697+KeishiFukuda@users.noreply.github.com>
2020-09-21 12:36:43 +0900

    全jspファイルをフォルダへ格納

---
+++ src/init/ageComfirmationInitServlet.java
@@ -0,0 +1,33 @@
+package init;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginInitServlet
+ */
+@WebServlet("/loginInitServlet")
+public class ageComfirmationInitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ageComfirmation.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
